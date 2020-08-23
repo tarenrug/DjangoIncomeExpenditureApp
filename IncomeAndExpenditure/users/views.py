9e22692a3194678d667a12cff52d330fb8cabd0b
tarenrug <taren_rug@hotmail.co.uk>
2020-08-23 01:43:59 +0100

    All fields saved in the Django Models class MyUserProfile

--- IncomeAndExpenditure/users/views.py
+++ IncomeAndExpenditure/users/views.py
@@ -7,14 +7,19 @@ def register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
         if form.is_valid():
+
             myuser = MyUserProfile()
             myuser.first_name = form.cleaned_data.get('first_name')
             myuser.last_name = form.cleaned_data.get('last_name')
+            myuser.email = form.cleaned_data.get('email')
+            myuser.phone_number = form.cleaned_data.get('phone_number')
             myuser.date_of_birth = form.cleaned_data.get('date_of_birth')
+            myuser.address_line_1 = form.cleaned_data.get('address_line_1')
+            myuser.address_line_2 = form.cleaned_data.get('address_line_2')
+            myuser.city = form.cleaned_data.get('city')
+            myuser.postcode = form.cleaned_data.get('postcode')
             myuser.username = form.cleaned_data.get('username')
-            # myuser.date_of_birth = form.cleaned_data.get('date_of_birth')
-            # myuser.first_name = form.cleaned_data.get('username')
-            # myuser.date_of_birth = form.cleaned_data.get('date_of_birth')
+            
             myuser.save()
             form.save()
             messages.success(request, f'Your account has been created! You are now able to login as')
